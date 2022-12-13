6e9b28e765973d64ec0dfa071f036f497f4e00d7
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2022-12-13 12:39:53 -0500

    13.12.2022

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("val").addEventListener("click", validar);
+
+function validar() {
+  const number = document.getElementById("registro").value;
+  const ocult = validator.maskify(number);
+  if (number.length < 16){
+    document.getElementById("registro").value=ocult;
+    alert("Este número es demasiado corto. Por favor, inserta un número válido.")
+  }
+  if (number.length > 16){
+    document.getElementById("registro").value=ocult;
+    alert("Este número es demasiado largo. Por favor, inserta un número válido.")
+  }
+  if (number.length === 16){
+    document.getElementById("registro").value=ocult;
+    const resultado = validator.isValid(number);
+    if (resultado === false){
+      alert ("Lo siento, el número de tarjeta " + ocult + " no es válido")
+    }
+    if (resultado === true){
+      alert ("El número de tarjeta " + ocult + " es válido. En un momento confirmaremos tu compra.")
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
 const validator = {
-  // ...
-};
+  
+  isValid: (tarjeta) => {
+    const length = tarjeta.length;
+    let count = 0;
+    // asegurarnos que la longitud sea par
+    if(length % 2 === 0)
+    {
+      //Comenzar a duplicar desde el primer número
+      for(let i = 0; i < length; i++)
+      {
+        let dig = parseInt(tarjeta[i]);
+        if (i % 2 === 0) // Sólo duplicaremos las posiciones impares
+        {
+          if ((dig *= 2) > 9) //Condicional si el número es de dos cifras
+          {
+            //separamos los números como dos variables
+            const cifra2 = dig % 10;
+            const cifra1 = parseInt(dig / 10);
+            // sumamos las cifras
+            dig = cifra1 + cifra2;
+          }
+        }
+        count += dig;
+      }
+    }
+    //El resultado debe ser múltiplo de 10
+    return (count % 10) === 0;
+  },
+
+  maskify: (tarjeta)=> {
+
+    const ult = tarjeta.substring(tarjeta.length - 4);
+    const inc = tarjeta.substring(0,tarjeta.length - 4);
+    const ocultar = inc.replace(/./g, "#") + ult;
+    return ocultar;
+  }
+}
+
 
 export default validator;
